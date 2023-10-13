b487101deedc023921b5698446880e498e9ad7e0
sow_mito <sow.mito@wardish.jp>
2023-10-13 12:02:32 +0900

    lecture1_list

---
+++ lecture1/lecture1_list.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct list {
+  struct list *next;
+  int data;
+};
+
+int main() {
+  struct list *head = NULL;
+  struct list *current = NULL;
+
+  for (int i = 1; i <= 100; i++) {
+    struct list *new = malloc(sizeof(struct list));
+    new->data = i;
+    new->next = NULL;
+
+    if (head == NULL) {
+      head = new;
+      current = new;
+    } else {
+      current->next = new;
+      current = new;
+    }
+  }
+
+  current = head;
+  while (current != NULL) {
+    printf("%d ", current->data);
+    current = current->next;
+  }
+}
